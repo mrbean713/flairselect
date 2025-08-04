6527f57c20d31a2e0d4738695ece400ee7cfc2d1
Nick <rsnick06@yahoo.com>
2025-08-04 16:57:51 -0700

    requests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,11 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off", // 👈 disable this one
+      // Optional: disable unused vars too
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off"
     },
   },
 ];
